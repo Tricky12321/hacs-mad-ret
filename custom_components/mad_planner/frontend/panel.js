9fb306f62b09aaae7915da5f904a36a4185b017c
Thue V. Iversen <thue.v.iversen@gmail.com>
2026-06-12 20:15:40 +0200

    Added missing files

---
+++ custom_components/mad_planner/frontend/panel.js
@@ -0,0 +1,43 @@
+class MadPlanPanel extends HTMLElement {
+  constructor() {
+    super();
+    this._iframe = null;
+    this._hass = null;
+    this._onMsg = this._onMsg.bind(this);
+  }
+
+  connectedCallback() {
+    if (this._iframe) return;
+    Object.assign(this.style, { display: 'block', width: '100%', height: '100%' });
+    this._iframe = document.createElement('iframe');
+    Object.assign(this._iframe.style, { width: '100%', height: '100%', border: 'none', display: 'block' });
+    this._iframe.src = '/mad-plan-static/index.html';
+    this.appendChild(this._iframe);
+    this._iframe.addEventListener('load', () => this._send());
+    window.addEventListener('message', this._onMsg);
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('message', this._onMsg);
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    this._send();
+  }
+
+  _send() {
+    const token = this._hass?.auth?.data?.access_token;
+    if (!token || !this._iframe?.contentWindow) return;
+    this._iframe.contentWindow.postMessage(
+      { type: 'mad_plan_auth', access_token: token },
+      window.location.origin
+    );
+  }
+
+  _onMsg(e) {
+    if (e.data?.type === 'mad_plan_request_auth') this._send();
+  }
+}
+
+customElements.define('mad-plan-panel', MadPlanPanel);
